0c4b5df4578059ced9398225195da39575702a29
hamayun <usmankhan7586@gmail.com>
2020-04-25 23:54:38 +0500

    abc

--- assets/jquery/custom.js
+++ assets/jquery/custom.js
@@ -21,8 +21,8 @@ $(".owl-carousel2").owlCarousel({
   loop: true,
   margin: 30,
   nav: true,
-  items: 3,
-  slideBy: 3,
+  items: 5,
+  slideBy: 5,
   dots: true,
   autoplay: false,
   autoplayTimeout: 5000,
@@ -36,6 +36,34 @@ $(".owl-carousel2").owlCarousel({
     },
   },
 });
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 function nextVideo(obj) {
   let elems = document.getElementsByClassName("customLabel");
   let forLabel = parseInt(obj.getAttribute("for").replace("s", ""));
